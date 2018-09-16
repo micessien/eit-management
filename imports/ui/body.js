cc45145070dda4bd847dc5a98b5f777136eada10
Micessien <micaeldie@gmail.com>
2018-09-16 21:14:28 +0000

    Add insert form

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,14 @@
 import { Template } from 'meteor/templating';
 
-import { Eits } from '../api/eits';
+import { Eits } from '../api/eits.js';
 
+// import './eit.js';
 import './body.html';
  
 Template.body.helpers({
     eits() {
-        return Eits.find({});
+        // Show newest eits at the top
+        return Eits.find({}, { sort: { createdAt: -1 } });
     },
 
 //   eits: [
@@ -14,4 +16,33 @@ Template.body.helpers({
 //     { firstname: 'Name 2', lastname: 'alexis', gender: 'Masculin', dateofbirth: '12/05/98' },
 //     { firstname: 'Name 3', lastname: 'alexis', gender: 'Masculin', dateofbirth: '12/05/98' },
 //   ],
+});
+
+Template.body.events({
+    'submit .new-eit'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const firstname = target.firstname.value;
+        const lastname = target.lastname.value;
+        const gender = target.gender.value;
+        const dateofbirth = target.dateofbirth.value;
+
+        // Insert a task into the collection
+        Eits.insert({
+            firstname,
+            lastname,
+            gender,
+            dateofbirth,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        target.firstname.value = '';
+        target.lastname.value = '';
+        target.gender.value = '';
+        target.dateofbirth.value = '';
+    },
 });
