3ad5e270b7e91e57b2f1cd333eef410fa51ccec5
Micessien <micaeldie@gmail.com>
2018-09-17 02:25:55 +0000

    connect users

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,16 +1,30 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Eits } from '../api/eits.js';
 
 import './eit.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
  
 Template.body.helpers({
     eits() {
-        // Show newest eits at the top
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            // If hide completed is checked, filter eits
+            return Eits.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the eits
         return Eits.find({}, { sort: { createdAt: -1 } });
     },
-    
+    incompleteCount() {
+        return Eits.find({ checked: { $ne: true } }).count();
+    },
+
 });
 
 Template.body.events({
@@ -32,6 +46,8 @@ Template.body.events({
             gender,
             dateofbirth,
             createdAt: new Date(), // current time
+            owner: Meteor.userId(),
+            username: Meteor.user().username,
         });
 
         // Clear form
@@ -40,4 +56,8 @@ Template.body.events({
         target.gender.value = '';
         target.dateofbirth.value = '';
     },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
+
 });
